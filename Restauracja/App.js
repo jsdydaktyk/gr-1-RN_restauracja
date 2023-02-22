7dae892bfe4e8bd7324e64e45df5d7549db4acad
jareks <jarek@eeww.pl>
2023-02-22 08:49:17 +0100

    Fix Stack error

--- Restauracja/App.js
+++ Restauracja/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
-import { createNativeStackNavigator } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from  '@react-navigation/native' ;
 
 const Stack = createNativeStackNavigator(); 
@@ -12,9 +12,10 @@ export default function App() {
       <StatusBar style="dark"/>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />          
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
         </Stack.Navigator>
       </NavigationContainer>
+      
     </>
   
   );
